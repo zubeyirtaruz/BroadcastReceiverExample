2b389455eb2a3fb1d7b76a11ce43393e72e2b8e6
Zubeyir Taruz <zubeyirtaruz@gmail.com>
2025-03-19 14:10:57 +0300

    Initial commit

---
+++ app/src/main/java/com/deepzub/broadcastreceiverexample/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.deepzub.broadcastreceiverexample
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.deepzub.broadcastreceiverexample.ui.theme.BroadcastReceiverExampleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            BroadcastReceiverExampleTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    BroadcastReceiverExampleTheme {
+        Greeting("Android")
+    }
+}
